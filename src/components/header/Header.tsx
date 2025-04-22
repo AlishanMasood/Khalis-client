6d5ae44a45be2d435696b6e9d42986c85b3ac7c8
AlishanMasood <alishanmasood20@gmail.com>
2025-04-22 05:21:40 +0500

    reassign issues fixed on deployment

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./header.module.css";
 import { Images } from "@/constants/images";
 import { Categories } from "@/constants/categories";
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,6 @@
-let bodyBackground =
+const bodyBackground =
   "https://khalispur.pk/wp-content/uploads/2021/10/background_image.png";
-let logo =
+const logo =
   "https://khalispur.pk/wp-content/uploads/2021/12/khalis-logo-svg.svg";
 export const Images = {
   logo,
